a9fb0f7828cf3ef71c2d562f6e3579da3d14517f
LilyNovikova <lil-nov@yandex.ru>
2019-12-19 00:31:46 +0300

    ...

--- NUnitTestProject1/NUnitTest1.cs
+++ NUnitTestProject1/NUnitTest1.cs
@@ -7,13 +7,11 @@ namespace NUnitTestProject1
     [TestFixture]
     public class TestSet
     {
-        [Test]
-        public void TestLess()
+        [TestCase("90,80,60,70,20", "50,50,60,30,20")]
+        public void TestLess(string input1, string input2)
         {
-            string s1 = "90,80,60,70,20";
-            string s2 = "50,50,60,30,20";
-            Set S1 = new Set("1", Utils.ToIntArray(s1.Split(','), ",")[0]);
-            Set S2 = new Set("2", Utils.ToIntArray(s2.Split(','), ",")[0]);
+            Set S1 = new Set("1", Utils.ToIntArray(input1.Split(','), ",")[0]);
+            Set S2 = new Set("2", Utils.ToIntArray(input2.Split(','), ",")[0]);
             Assert.IsTrue(S2.Less(S1), "S2 is not less than S1");
         }
     }
@@ -21,23 +19,28 @@ namespace NUnitTestProject1
     [TestFixture]
     public class TestTable
     {
-        [Test]
-        public void TestOptimize()
+        [TestCase ("input.txt", " ,\n")]
+        public void TestOptimize(string InputFile, string SplitSymbols)
         {
-            string[] names = { "1", "2", "3", "4", "5", "6", "7", "8" };
-            int[][] data = Utils.ToIntArray(FileUtils.ReadText("input.txt"), " ,\n");
+
+            int[][] data = Utils.ToIntArray(FileUtils.ReadText(InputFile), SplitSymbols);
+            string[] names = new string[data.Length];
+            for (int k = 0; k < names.Length; k++)
+            {
+                names[k] = (k + 1).ToString();
+            }
             Table table = new Table(names, data);
 
             table.Optimize();
             int i = 1;
-            foreach(Set S in table)
+            foreach (Set S in table)
             {
-                Assert.IsTrue(int.Parse(S.name) < 7,string.Format( "Row #{0} wasn't deleted", S.name));
+                Assert.IsTrue(int.Parse(S.name) < 7, string.Format("Row #{0} wasn't deleted", S.name));
                 Assert.IsTrue(i == int.Parse(S.name), string.Format("Row #{0} was deleted", S.name));
                 i++;
             }
 
-            
+
         }
     }
 }
